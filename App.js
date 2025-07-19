831d85a06bff3496b56281e9e7905aa6cc4f83fd
Aqdas30 <aqdaszulfiqar30@gmail.com>
2025-07-19 22:48:06 +0500

    lock file6

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Details from "./src/Details";
+import Dashboard from "./src/Dashboard";
+import Login from "./src/Login";
 
-export default function App() {
+
+
+const Stack =createStackNavigator();
+
+
+
+export default function App(){
+
   return (
-    <View style={styles.container}>
-      <Text>hello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login"
+        component={Login}
+        options={{headerShown: false}}
+        />
+
+         <Stack.Screen name="Details"
+        component={Details}
+        options={{headerShown: false}}
+        />
+         <Stack.Screen name="Dashboard"
+        component={Dashboard}
+        options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
